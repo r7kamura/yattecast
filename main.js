3a8c421be6661c76322187b0d89ca66a1482e13c
Shinta Koyanagi <yancya@upec.jp>
2018-09-19 13:51:01 +0900

    Default display:none for articles

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 window.addEventListener('load', () => {
   document.querySelectorAll('.main > .container > .card > article').forEach(e => {
     const d = Date.parse(e.querySelector('.date-time').innerHTML.trim());
-    if (d > Date.now()) {
-      e.style.display = 'none';
+    
+    if (d < Date.now()) {
+      e.style.display = '';
+    } else {
       console.log('Not yet publish it.');
     }
   });
