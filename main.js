3080597b5a4d9ee48924eafeb2a206105a8f336e
Shinta Koyanagi <yancya@upec.jp>
2018-09-19 13:46:17 +0900

    display none not yet published post

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+  document.querySelectorAll('.main > .container > .card > article').forEach(e => {
+    const d = Date.parse(e.querySelector('.date-time').innerHTML.trim());
+    if (d > Date.now()) {
+      e.style.display = 'none';
+      console.log('Not yet publish it.');
+    }
+  });
+});
