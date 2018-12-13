e22e2f849d7c63cf2027fe8c3897888651e1af89
Naokazu Terada <naokazu.terada@gmail.com>
2018-12-13 22:31:07 +0900

    close #17: Show MP3 file size

--- _plugins/customfilters.rb
+++ _plugins/customfilters.rb
@@ -1,4 +1,5 @@
 require 'faraday'
+require 'filesize'
 
 module Jekyll
   module CustomFilters
@@ -6,9 +7,13 @@ module Jekyll
       File.extname(filepath)[1..-1]
     end
 
-    def filesize(url)
+    def filesize(url, pretty=false)
       res = Faraday.head url
       res.headers['Content-Length']
+    end
+
+    def pretty_size(size)
+      (Filesize.from('0 KB') + Filesize.from(size+' B')).pretty
     end
   end
 end
